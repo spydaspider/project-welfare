ff297d85bbafbbd193def4f637c3d345db122fa2
spydaspider <spydaspider@gmail.com>
2022-10-25 02:15:21 +0000

    Add registration form

---
+++ src/helpers/createMember.js
@@ -0,0 +1,27 @@
+export default class CreateMember{
+    constructor(membershipNumber,appName,staffNumber,district,telephone,monthlySavings,witnessName,witnessContact,nomineeName1,nomineeName2,nomineeName3,nRelationship1,nRelationship2,nRelationship3,nPercentage1,nPercentage2,nPercentage3,date,time)
+    {
+        this.membershipNumber = membershipNumber;
+        this.appName = appName;
+        this.staffNumber = staffNumber;
+        this.district = district;
+        this.telephone = telephone;
+        this.monthlySavings = monthlySavings;
+        this.witnessName = witnessName;
+        this.witnessContact = witnessContact;
+        this.nomineeName1 = nomineeName1;
+        this.nomineeName2 = nomineeName2;
+        this.nomineeName3 = nomineeName3;
+        this.nRelationship1 = nRelationship1;
+        this.nRelationship2 = nRelationship2;
+        this.nRelationship3 = nRelationship3;
+        this.nPercentage1 = nPercentage1;
+        this.nPercentage2 = nPercentage2;
+        this.nPercentage3 = nPercentage3;
+        this.date = date;
+        this.time = time;
+
+
+
+    }
+}
--- src/membership.js
+++ src/membership.js
@@ -1,9 +1,169 @@
+import {useState} from 'react';
+import useFetch from './useFetch.js';
+import CreateMember from './helpers/createMember.js';
 const Membership = () =>{
+    const [applicantName,setApplicantName] = useState('');
+    const [staffNumber,setStaffNumber] = useState('');
+    const [district,setDistrict] = useState('');
+    const [telephone, setTelephone] = useState('');
+    const [monthlySavings,setMonthlySavings] = useState('');
+    const [witnessName, setWitnessName] = useState('');
+    const [witnessContact, setWitnessedContact] = useState('');
+    const [nomineeName1,setNomineeName1] = useState('');
+    const [nomineeName2,setNomineeName2] = useState('');
+    const [nomineeName3,setNomineeName3] = useState('');
+    const [nRelationship1,setNRelationship1] = useState('');
+    const [nRelationship2,setNRelationship2] = useState('');
+    const [nRelationship3, setNRelationship3] = useState('');
+    const [nPercentage1,setNPercentage1] = useState('');
+    const [nPercentage2,setNPercentage2] = useState('');
+    const [nPercentage3,setNPercentage3] = useState('');
+    const [negativeNumber,setNegativeNumber] = useState(null);
+    const {data:members} = useFetch('http://localhost:8050/Members');
+    const [error,setError] = useState(null);
+    const [staffNumberExists, setStaffNumberExists] = useState(null);
+    const handleSubmit = (e) =>{
+e.preventDefault();
+let dateTime = new Date();
+let time = dateTime.toISOString().split('T')[1];
+let date = dateTime.toISOString().split('T')[0];
+let searchCounter = 0;
+console.log(members.length);
+if(members && members.length !== 0)
+{
+    members.forEach((member)=>{
+        if(member.staffNumber.toLowerCase() === staffNumber.toLowerCase())
+        {
+             searchCounter = searchCounter + 1;
+        }
+    })
+    if(searchCounter === 0)
+    {
+        if(nPercentage1 < 0 || nPercentage2 < 0 || nPercentage3 < 0 || monthlySavings < 0)
+        {
+            setNegativeNumber(true);
+        }
+        else{
+        //save member
+        setNegativeNumber(null);
+        setError(null);
+        const member = new CreateMember(members.length+1,applicantName,staffNumber,district,telephone,monthlySavings,witnessName,witnessContact,nomineeName1,nomineeName2,nomineeName3,nRelationship1,nRelationship2,nRelationship3,nPercentage1,nPercentage2,nPercentage3,date,time);
+
+        //send to the database.
+        fetch('http://localhost:8050/Members',{
+            method: 'POST',
+            headers: {"Content-type": "Application/json"},
+            body: JSON.stringify(member)
+        }).then(()=>{
+            window.location.reload();
+        }).catch((err)=>{
+            setError(err.message);
+        })
+        }
+    }
+    else
+    {
+        setStaffNumberExists(true);
+        setNegativeNumber(null);
+        setError(null);
+    }
+    }
+    else{
+        if(nPercentage1 < 0 || nPercentage2 < 0 || nPercentage3 < 0 || monthlySavings < 0)
+        {
+            setNegativeNumber(true);
+        }
+        else
+        {
+            setNegativeNumber(null);
+            setError(null);
+            const member = new CreateMember(1,applicantName,staffNumber,district,telephone,monthlySavings,witnessName,witnessContact,nomineeName1,nomineeName2,nomineeName3,nRelationship1,nRelationship2,nRelationship3,nPercentage1,nPercentage2,nPercentage3,date,time);
+
+            fetch('http://localhost:8050/Members',{
+                method: 'POST',
+                headers: {"Content-type": "Application/json"},
+                body: JSON.stringify(member)
+            }).then(()=>{
+                window.location.reload();
+            }).catch((err)=>{
+                setError(err.message);
+
+            })
+        
+        }
+    }
+      
+    }
     return(
         <div className = "membership-form">
             <h1>Membership form.</h1>
-            <form>                
+            {error && <p className = "error">{error}</p>}
+            {staffNumberExists&& <p className = "error">Staff number already exists.</p>}
+            {negativeNumber&&<p className = "error">Negative number detected.</p>}
+            <form onSubmit = {handleSubmit}>                
+                <fieldset className = "field-set">
+                    <legend className = "label-style">Applicants Name</legend>
+                    <input type = "text" value = {applicantName} onChange = {(e)=>setApplicantName(e.target.value)} required/>
+                </fieldset>
+                <fieldset className = "field-set">
+                    <legend className = "label-style">Staff Number</legend>
+                    <input type = "text" value = {staffNumber} onChange = {(e)=>setStaffNumber(e.target.value)} required/>
+
+                </fieldset>
+                <fieldset className = "field-set">
+                    <legend className = "label-style">District/Station</legend>
+                    <input type = "text" value = {district} onChange = {(e)=>setDistrict(e.target.value)} required/>
+                </fieldset>
+                <fieldset className = "field-set">
+                    <legend className = "label-style">Telephone</legend>
+                    <input type = "text" value = {telephone} onChange = {(e)=>setTelephone(e.target.value)} required/>
+                </fieldset>
+                <fieldset className = "field-set">
+                    <legend className = "label-style">Monthly Savings Amount</legend>
+                    <input type = "number" value = {monthlySavings} onChange = {(e)=>setMonthlySavings(e.target.value)}/>
+                </fieldset>
+                <h2>Nominee</h2>
+                <table>
+                    <thead>
+                    <tr>
+                    <th>S/No</th>
+                    <th>Name</th>
+                    <th>Relationship</th>
+                    <th>Percentage</th>
+                    </tr>
+                    </thead>
+                    <tbody>
+                    <tr>
+                        <td>1.</td>
+                        <td><input type = "text" value = {nomineeName1} onChange = {(e)=>setNomineeName1(e.target.value)}/></td>
+                        <td><input type = "text" value = {nRelationship1} onChange = {(e)=>setNRelationship1(e.target.value)}/></td>
+                        <td><input type = "number" value = {nPercentage1} onChange = {(e)=>setNPercentage1(e.target.value)}/></td>
+                    </tr>
+                    <tr>
+                    <td>2.</td>
+                        <td><input type = "text" value = {nomineeName2} onChange = {(e)=>setNomineeName2(e.target.value)}/></td>
+                        <td><input type = "text" value = {nRelationship2} onChange = {(e)=>setNRelationship2(e.target.value)}/></td>
+                        <td><input type = "number" value = {nPercentage2} onChange = {(e)=>setNPercentage2(e.target.value)}/></td>
+                    </tr>
+                    <tr>
+                    <td>3.</td>
+                        <td><input type = "text" value = {nomineeName3} onChange = {(e)=>setNomineeName3(e.target.value)}/></td>
+                        <td><input type = "text" value = {nRelationship3} onChange = {(e)=>setNRelationship3(e.target.value)}/></td>
+                        <td><input type = "number" value = {nPercentage3} onChange = {(e)=>setNPercentage3(e.target.value)}/></td>
+                    </tr>
+                    </tbody>
                 
+                </table>
+                <fieldset className = "field-set">
+                    <legend>Witnessed Name</legend>
+                    <input type = "text" value = {witnessName} onChange = {(e)=>setWitnessName(e.target.value)} required/>
+                </fieldset>
+                <fieldset>
+                    <legend className = "label-style">Witness Contact</legend>
+                    <input type = "text" value = {witnessContact} onChange = {(e)=>setWitnessedContact(e.target.value)} required/>
+
+                </fieldset>
+                <button className = "register-button">Register Member</button>
             </form>
         </div>
     )
