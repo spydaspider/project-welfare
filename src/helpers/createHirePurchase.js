b514f2d4b9400e7e4ac54713b5060fbba6cc1642
spydaspider <spydaspider@gmail.com>
2022-11-27 14:04:30 +0000

    Add hirepusrchase

--- src/helpers/createHirePurchase.js
+++ src/helpers/createHirePurchase.js
@@ -14,8 +14,5 @@ export default class CreateHirePurchase{
         this.duration = duration;
         this.date = date;
         this.time = time;
-
-
-
-    }
+      }
 }
--- src/hirePurchase.js
+++ src/hirePurchase.js
@@ -1,11 +1,54 @@
 import Navigation from "./nav"
 import SecondNavigation from "./nav2"
-
+import useFetch from "./useFetch";
 const HirePurchases = () =>{
+    const {data: hirePurchases, isPending: isLoading, error} = useFetch('http://localhost:8050/hirePurchases');
     return(
-        <div className = "HirePurchases-wrapper">
+        <div className = "hirePurchase-wrapper">
             <SecondNavigation/>
             <Navigation/>
+            <div className = "hire-purchase">
+               <h1>Purchases</h1>
+               <table>
+                <thead>
+                   <tr>
+                    <th>Applicant Name</th>
+                    <th>Staff Number</th>
+                    <th>District</th>
+                    <th>Telephone</th>
+                    <th>Item</th>
+                    <th>Unit Price</th>
+                    <th>Total Amount</th>
+                    <th>Brand</th>
+                    <th>Quantity</th>
+                    <th>Duration</th>
+                    <th>Date</th>
+
+
+                   </tr>
+                </thead>
+                <tbody>
+                    {hirePurchases && hirePurchases.map((hp)=>(
+                         <tr key = {hp.id}>
+                            <td>{hp.appName}</td>
+                            <td>{hp.staffNumber}</td>
+                            <td>{hp.district}</td>
+                            <td>{hp.telephone}</td>
+                            <td>{hp.itemName}</td>
+                            <td>{Number(hp.unitPrice).toFixed(2)}cedis</td>
+                            <td>{Number(hp.totalAmount).toFixed(2)}cedis</td>
+                            <td>{hp.itemBrand}</td>
+                            <td>{hp.quantity}</td>
+                            <td>{hp.duration}</td>
+                            <td>{hp.date}</td>
+                         </tr>
+                    ))
+                    
+                      }
+
+                </tbody>
+               </table>
+            </div>
         </div>
     )
 }
