a17104e08d68b18b6cd4d9374e8daed78790dd68
spydaspider <spydaspider@gmail.com>
2022-11-29 23:35:54 +0000

    Add Update links

--- src/nav.js
+++ src/nav.js
@@ -1,21 +1,25 @@
-import { Link } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 import { useHistory } from 'react-router-dom';
 import graIcon from './images/gra.png';
 const Navigation = () =>{
-    
+    const navLinkStyles = ({ isActive }) =>{
+      return{
+         backgroundColor: isActive ? 'blue':'',
+         textDecoration: isActive ? 'underline':'none',
+      }
+    }    
    return(
 
     <div className = "nav-bar">
-       <Link to = "/search"> <img className = "gra-icon" src = {graIcon} alt = "gra icon"/></Link>
+       <NavLink to = "/search"> <img className = "gra-icon" src = {graIcon} alt = "gra icon"/></NavLink>
         <div className = "links">
-        <Link to = "/membership">Membership</Link>
-        <Link to = "/search">Search</Link>
-        <Link to = "/beneficiaries">Beneficiaries</Link>
-        <Link to = "/loans">Loans</Link>
-        <Link to = "/income">Income</Link>
-        <Link to = "/deductions">Deductions</Link>
-        <Link to = "/hirePurchase">Purchase</Link>
-      
+        <NavLink style = {navLinkStyles} to = "/membership">Membership</NavLink>
+        <NavLink style = {navLinkStyles} to = "/search">Search</NavLink>
+        <NavLink style = {navLinkStyles} to = "/beneficiaries">Beneficiaries</NavLink>
+        <NavLink style = {navLinkStyles} to = "/loans">Loans</NavLink>
+        <NavLink style = {navLinkStyles} to = "/income">Income</NavLink>
+        <NavLink style = {navLinkStyles} to = "/deductions">Deductions</NavLink>
+        <NavLink style = {navLinkStyles} to = "/hirePurchase">Purchase</NavLink>
         </div>
         </div>
         
